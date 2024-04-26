5915cfc4902130ded0eed804b8c195d0d25ca2e4
mateus.hemkemeier <mateuslealhemkemeier@gmail.com>
2024-04-26 10:03:28 -0300

    [style] - Doc arquivos.txt.lerEscrever - Renomeado lerEscreverTxt.groovy -> lerEscrever.groovy

--- arquivos/posicoes/lerEscrever.groovy
+++ arquivos/posicoes/lerEscrever.groovy
@@ -1,11 +1,11 @@
 
 
 def List buscaDadosAquivoPos(Map posicoes,ignorarDelimitador, arquivoFuncao) {
-    /*
-  ** posicoes: deve receber um mapa sendo a chave o nome da propriedade que devemos nomear o obj, e o valor sendo um int do tamanho do campo
-  ** ignorarDelimitador: se receber false continua seguindo o padrão estabelicido acima, caso contrario busca a ocorrencia do delimitador no final de cada posição.
-  ** arquivo: instancia da classe Arquivo, utilizando a funcao Arquivo.ler()
-  ** return: Lista de HashMaps, sendo as chaves definidas no parametro posicoes.
+  /*
+  * posicoes: deve receber um mapa sendo a chave o nome da propriedade que devemos nomear o obj, e o valor sendo um int do tamanho do campo
+  * ignorarDelimitador: se receber false continua seguindo o padrão estabelicido acima, caso contrario busca a ocorrencia do delimitador no final de cada posição.
+  * arquivo: instancia da classe Arquivo, utilizando a funcao Arquivo.ler()
+  * return: Lista de HashMaps, sendo as chaves definidas no parametro posicoes.
   * ex :
   * Map dicionario = [
   *     e_num_ainf:6,
